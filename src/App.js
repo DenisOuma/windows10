62af8b2792983e1fa1a9f8aec17e6195245871cf
Denis Ouma <dennisouma38@gmail.com>
2023-04-29 12:36:32 +0300

    Update on the application dashboard setup and routes page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { LoginPage } from "./pages/LoginPage";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<div className="App bg-primary-bg-page">
+				<Router>
+					<>
+						<Routes>
+							<Route exact path="/login" element={<LoginPage />} />
+						</Routes>
+					</>
+				</Router>
+			</div>
+		</div>
+	);
 }
 
 export default App;
